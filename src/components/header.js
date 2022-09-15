260862552eb8a937f9ffe29d0a732a25fd34db10
RIFZKI ADIYAKSA <29408949+rifzkiadiyaksa@users.noreply.github.com>
2022-09-15 11:41:21 +0700

    Edit Header

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ function Header() {
   return (
     <header className="sticky top-0 w-full p-4 flex justify-between items-center bg-white dark:bg-primary border-b border-gray-200 dark:border-secondary z-10">
       <h1 className="text-2xl font-nunito flex font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 via-fuchsia-400 to-blue-400">
-        Link.gen
+        LinKu
       </h1>
       <div className="flex justify-center items-center space-x-4">
         {dark ? (
@@ -31,12 +31,12 @@ function Header() {
           />
         )}
         <a
-          rel="noreferrer"
-          href="https://linkpile-bffd7.web.app/"
+          rel="my site"
+          href="https://rifzki.my.id/"
           target="_blank"
           className="px-4 py-2 text-white font-nunito font-bold rounded-3xl bg-gradient-to-r from-primary-accent to-rose-400 hover:bg-secondary-accent"
         >
-          Try Linkpile
+          My Site
         </a>
       </div>
     </header>
